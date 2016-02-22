c1aa694103a489fd01173ba490e5d3c8927ed2a7
Mr Chen <236514977@qq.com>
2016-02-22 18:40:30 +0800

    UI switchable 增加

--- jquery.v.slider/1.0.1/jquery.ui.switchable.js
+++ jquery.v.slider/1.0.1/jquery.ui.switchable.js
@@ -1,7 +1,6 @@
 /* developer: MrChen
  jquery.ui.switchable  */
 
-
 !(function (factory) {
     if (typeof define === "function" && define.amd) {
         // AMD模式
@@ -80,48 +79,36 @@
     Switchable.DEFAULTS = {
         type: "tab",//类型
         direction: "left",   //和slider 相关的属性 "left" ,"top"
-
         contentClass: "ui-switchable-panel-main", //面板包含元素
         mainClass: "ui-switchable-panel",//面板 元素
         mainSelectedClass: "ui-switchable-panel-selected", //面板 当前 活动 元素
-
         bodyClass: "ui-switchable-panel-body",//面板内容 包含的 元素  mainClass 的父级元素
-
         navClass: "ui-switchable-trigger",//导航 包含元素   [bool ： 判断是否需要插入 navClass]
         navItem: "ui-switchable-item",//导航元素
         navSelectedClass: "ui-switchable-selected",//导航活动当前元素
-
         prevClass: "ui-switchable-prev", //上一页
         nextClass: "ui-switchable-next",  // 下一页
         contentPage: "ui-switchable-page", //箭头包裹       _this.el.append($navClass);
-
-
         bodyExtra: false, //"ui-switchable-extra"
-
-
         isAutoPlay: !1, //是否自动播放
         mouseenterStopPlay: !0, //鼠标进入停滞播放
         playDirection: "next", //播放的方向
-
         event: "mouseover", // 鼠标移入navItem 的事件
         speed: 400,         // 动画播放的时间
-
         delay: 150,         //切换 延迟时间,保证快速 切换 不闪动
         defaultPanel: 0,    //默认面板 初始索引 [索引值]
-
         stayTime: 5e3,  //停留时间
-
         includeMargin: !1, //计算 元素 单位的时候 是否计算 margin 值
-
         width: 0,
         height: 0,
         step: 1,        //  每次走的 步数
         seamlessLoop: !1, //无缝循环
-
         visible: 1,     // 显示 的个数 { 针对 slider}
         easing: "swing",
         hasLoop: !1,
         counter: 1, // 索引 计数器 [ false]
+        hasSetup:!1, //是否允许
+        hasArrow: !1,
         callback: null,
         onNext: null,
         onPrev: null
